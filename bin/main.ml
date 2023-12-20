699f30ff7c1be0e653a740844d649193173ea6a4
MatheusGh0st <matheusghost88@gmail.com>
2023-12-20 12:19:37 -0300

    add new function and add types

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+type position = { line: int; column: int } [@@warning "-69"]
+
+type piece_color = Black | White | None [@@warning "-37"]
+
+type piece = { position: position; symbol: string } [@@warning "-69"]
+
+let default_position = { line = 0; column = 0}
+
+let default_piece = { position = default_position; color = None; symbol = "-" }
+
+let create_table (rows: int) (cols: int) : table =
+  let create_row (n: int) : piece array =
+    Array.init n (fun _ -> default_piece)
+  in
+  { piece_matrix = Array.init rows (fun _ -> create_row cols) }
+
+  
